bd1a56d64c2d9195fbfe29a01fbca6fb2fb89d70
ishan pradeep kumara <ishans@insharptechnologies.com>
2019-07-19 12:02:28 +0530

    commit 2019-07-19

--- front-end-project/src/app/app.module.ts
+++ front-end-project/src/app/app.module.ts
@@ -3,17 +3,34 @@ import { NgModule } from '@angular/core';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppRoutingModule } from './app-routing.module';
+import {Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CategoryComponent } from './category/category.component';
 import { PostComponent } from './post/post.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
-import {Route} from "@angular/router";
+import { UserComponent } from './user/user.component';
+import { NalaviliGeeComponent } from './nalavili-gee/nalavili-gee.component';
+import { PurwaGarbaniComponent } from './purwa-garbani/purwa-garbani.component';
+import { GarbaniComponent } from './garbani/garbani.component';
+import { BilidaComponent } from './bilida/bilida.component';
+import { PunchiPuthaComponent } from './punchi-putha/punchi-putha.component';
+import { ContactComponent } from './contact/contact.component';
 
-const route : Route =[
-  { path : '', com}
-  ];
+const routes: Routes = [
+  { path : 'home', component : HomeComponent},
+  { path : 'category', component : CategoryComponent},
+  { path : 'user', component : UserComponent},
+  { path : 'post', component : PostComponent},
+  { path : 'nalaviliGee', component : NalaviliGeeComponent},
+  { path : 'purwaGarbani', component : PurwaGarbaniComponent},
+  { path : 'garbani', component : GarbaniComponent},
+  { path : 'bilida', component : BilidaComponent},
+  { path : 'punchiPutha', component : PunchiPuthaComponent},
+  { path : 'contact', component : ContactComponent},
+
+];
 
 @NgModule({
   declarations: [
@@ -22,12 +39,20 @@ const route : Route =[
     CategoryComponent,
     PostComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    UserComponent,
+    NalaviliGeeComponent,
+    PurwaGarbaniComponent,
+    GarbaniComponent,
+    BilidaComponent,
+    PunchiPuthaComponent,
+    ContactComponent
   ],
   imports: [
     AngularFontAwesomeModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
